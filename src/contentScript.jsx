7c8a487fc38ac7630ecf14e8905db9abbe5057ec
Ankit <ankitchauhan21500@gmail.com>
2024-12-12 17:40:18 +0530

    extension published :)

--- src/contentScript.jsx
+++ src/contentScript.jsx
@@ -1,17 +1,4 @@
-
-console.log("Linkedin Editor Loaded");
-const boldMap = {
-    'a': '𝗮', 'b': '𝗯', 'c': '𝗰', 'd': '𝗱', 'e': '𝗲', 'f': '𝗳', 'g': '𝗴', 'h': '𝗵', 'i': '𝗶',
-    'j': '𝗷', 'k': '𝗸', 'l': '𝗹', 'm': '𝗺', 'n': '𝗻', 'o': '𝗼', 'p': '𝗽', 'q': '𝗾', 'r': '𝗿', 's': '𝘀',
-    't': '𝘁', 'u': '𝘂', 'v': '𝘃', 'w': '𝘄', 'x': '𝘅', 'y': '𝘆', 'z': '𝘇',
-    'A': '𝗔', 'B': '𝗕', 'C': '𝗖', 'D': '𝗗', 'E': '𝗘', 'F': '𝗙', 'G': '𝗚', 'H': '𝗛', 'I': '𝗜',
-    'J': '𝗝', 'K': '𝗞', 'L': '𝗟', 'M': '𝗠', 'N': '𝗡', 'O': '𝗢', 'P': '𝗣', 'Q': '𝗤', 'R': '𝗥', 'S': '𝗦',
-    'T': '𝗧', 'U': '𝗨', 'V': '𝗩', 'W': '𝗪', 'X': '𝗫', 'Y': '𝗬', 'Z': '𝗭',
-    '0': '𝟬', '1': '𝟭', '2': '𝟮', '3': '𝟯', '4': '𝟰', '5': '𝟱', '6': '𝟲', '7': '𝟳', '8': '𝟴', '9': '𝟵',
-    '!': '!', '@': '@', '#': '#', '$': '$', '%': '%', '^': '^', '&': '&', '*': '*', '(': '(', ')': ')',
-    '-': '-', '=': '=', '+': '+', '[': '[', ']': ']', '{': '{', '}': '}', '\\': '\\', '|': '|', ';': ';',
-    ':': ':', '"': '"', '\'': '\'', ',': ',', '.': '.', '/': '/', '<': '<', '>': '>',
-};
+chat
 
 const italicMap = {
     'a': '𝑎', 'b': '𝑏', 'c': '𝑐', 'd': '𝑑', 'e': '𝑒', 'f': '𝑓', 'g': '𝑔', 'h': 'ℎ', 'i': '𝑖',
@@ -27,6 +14,22 @@ const italicMap = {
     '}': '｝', '~': '～', '€': '€', '£': '£', '¥': '¥', '¢': '¢', '©': '©', '®': '®', '™': '™'
 };
 
+console.log("LinkedIn Editor Loaded");
+
+const underlineMap = {
+    'a': 'a\u0332', 'b': 'b\u0332', 'c': 'c\u0332', 'd': 'd\u0332', 'e': 'e\u0332', 'f': 'f\u0332', 'g': 'g\u0332', 'h': 'h\u0332', 'i': 'i\u0332',
+    'j': 'j\u0332', 'k': 'k\u0332', 'l': 'l\u0332', 'm': 'm\u0332', 'n': 'n\u0332', 'o': 'o\u0332', 'p': 'p\u0332', 'q': 'q\u0332', 'r': 'r\u0332', 's': 's\u0332',
+    't': 't\u0332', 'u': 'u\u0332', 'v': 'v\u0332', 'w': 'w\u0332', 'x': 'x\u0332', 'y': 'y\u0332', 'z': 'z\u0332',
+    'A': 'A\u0332', 'B': 'B\u0332', 'C': 'C\u0332', 'D': 'D\u0332', 'E': 'E\u0332', 'F': 'F\u0332', 'G': 'G\u0332', 'H': 'H\u0332', 'I': 'I\u0332',
+    'J': 'J\u0332', 'K': 'K\u0332', 'L': 'L\u0332', 'M': 'M\u0332', 'N': 'N\u0332', 'O': 'O\u0332', 'P': 'P\u0332', 'Q': 'Q\u0332', 'R': 'R\u0332', 'S': 'S\u0332',
+    'T': 'T\u0332', 'U': 'U\u0332', 'V': 'V\u0332', 'W': 'W\u0332', 'X': 'X\u0332', 'Y': 'Y\u0332', 'Z': 'Z\u0332',
+    '0': '0\u0332', '1': '1\u0332', '2': '2\u0332', '3': '3\u0332', '4': '4\u0332', '5': '5\u0332', '6': '6\u0332', '7': '7\u0332', '8': '8\u0332', '9': '9\u0332',
+    '!': '!̲', '@': '@̲', '#': '#̲', '$': '$̲', '%': '%̲', '^': '^̲', '&': '&̲', '*': '*̲', '(': '(̲', ')': ')̲',
+    '-': '-̲', '=': '=̲', '+': '+̲', '[': '[̲', ']': ']̲', '{': '{̲', '}': '}̲', '\\': '\\̲', '|': '|̲', ';': ';̲',
+    ':': ':̲', '"': '"̲', '\'': '\'̲', ',': ',̲', '.': '.̲', '/': '/̲', '<': '<̲', '>': '>̲'
+};
+
+
 
 function mapToBoldUnicode(text) {
     return text.split('').map(char => boldMap[char] || char).join('');
@@ -36,7 +39,7 @@ function mapToItalicUnicode(text) {
     return text.split('').map(char => italicMap[char] || char).join('');
 }
 
-function applyBulletList() {
+function applyBulletList(text) {
     const selection = window.getSelection();
     const range = selection.getRangeAt(0);
     const selectedText = selection.toString();
@@ -114,6 +117,30 @@ function applyItalic() {
     selection.addRange(newRange);
 }
 
+
+function applyUnderline() {
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+    const selectedText = selection.toString();
+    const italicText = underlineMap(selectedText);
+
+    // todo :: Delete the selected text
+    range.deleteContents();
+
+    // todo :: Insert the italic-styled text
+    const newNode = document.createTextNode(italicText);
+    range.insertNode(newNode);
+
+    // todo :: cursor moved to the end
+    const newRange = document.createRange();
+    newRange.setStart(newNode, newNode.length);
+    newRange.setEnd(newNode, newNode.length);
+
+    // todo :: clear the current selection
+    selection.removeAllRanges();
+    selection.addRange(newRange);
+}
+
 // https://www.linkedin.com/in/ankitchauhan21/overlay/create-post/
 
 
@@ -134,5 +161,10 @@ document.addEventListener('keydown', (event) => {
     if (event.ctrlKey && event.key === 'm') {
         console.log("Ctrl + m");
         applyNumberList();
+    }
+
+    if (event.altKey && event.key === 'u') {
+        console.log("Ctrl + u");
+        applyUnderline();
     }
 });
